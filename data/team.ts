32ad1b1302f000b2b421c0ffa41c9baf0b9e3ff6
gymasao <90744935+gymasao@users.noreply.github.com>
2026-08-24 23:06:33 +0900

    feat: add 2026 season statistics pages

---
+++ data/team.ts
@@ -0,0 +1,55 @@
+export const teamSummary = {
+  games: 27, wins: 13, losses: 11, draws: 3, winningPercentage: ".481",
+  runs: 145, runsAllowed: 115, battingAverage: ".262", era: "3.904", homeRuns: 10, stolenBases: 74,
+};
+
+export const games = [
+  ["02.07", "練習試合", "エンパワーズ", "夢の島9面", "×", "https://www.youtube.com/watch?v=_HgaRJeBdpA"],
+  ["02.14", "練習試合", "KRB SATURN", "夢の島3面", "○", ""],
+  ["02.21", "練習試合", "トゥモローネバーノーズ", "夢の島12面", "○", ""],
+  ["02.28", "練習試合", "エンパワーズ", "夢の島10面", "○", "https://youtu.be/HbiXy9DnFDQ"],
+  ["03.07", "練習試合", "Hustle Japan", "猿江恩賜公園", "×", "https://youtu.be/BO-MapI44F4"],
+  ["03.14", "練習試合", "Hustle Japan", "夢の島10面", "×", "https://www.youtube.com/watch?v=FADj4rr5hm0"],
+  ["03.21", "練習試合", "江戸川キャットハンズ", "夢の島4面", "×", "https://www.youtube.com/watch?v=_7vyViOaeIc"],
+  ["04.04", "練習試合", "東京桐蔭", "猿江恩賜公園", "○", "https://youtu.be/DRcEZs89urI"],
+  ["04.11", "大会", "NEC", "三郷38面", "○", "https://www.youtube.com/watch?v=NBDmU43iahI"],
+  ["04.18", "練習試合", "明海ダービー倶楽部", "夢の島10面", "○", "https://www.youtube.com/watch?v=9Dv3EpewB8I"],
+  ["04.25", "大会", "豊田通商", "三郷38面", "○", "https://www.youtube.com/watch?v=jISpPuJQP_Y"],
+  ["05.02", "練習試合", "江戸川キャットハンズ", "夢の島7面", "△", "https://youtu.be/v5ruHYmaJVw"],
+  ["05.09", "練習試合", "ダークス", "篠崎公園B地区1面", "○", "https://youtu.be/eCaKxZbnHUs"],
+  ["05.17-1", "大会", "エイエイエス", "三郷36面", "○", "https://youtu.be/MWRqgL8rO14"],
+  ["05.17-2", "大会", "東洋水産", "三郷38面", "○", "https://youtu.be/3ZGW-siM1-w"],
+  ["05.23", "練習試合", "docomo", "夢の島7面", "△", "https://www.youtube.com/watch?v=4slQHbZbYRU"],
+  ["05.30", "練習試合", "docomo", "夢の島7面", "×", "https://youtu.be/ndoftsNZZKo"],
+  ["06.07", "大会", "目黒区代表 THK株式会社", "上井草B面", "×", "https://www.youtube.com/watch?v=pCrnIblHIZo"],
+  ["06.13", "練習試合", "横浜ミラクルズ", "夢の島7面", "○", "https://www.youtube.com/watch?v=WahGWyj3pBw"],
+  ["06.20", "練習試合", "中央マリナーズ", "夢の島10面", "×", "https://www.youtube.com/watch?v=K5_p6OzoKYA"],
+  ["07.04", "練習試合", "ピーチボーイズ", "夢の島10面", "×", "https://www.youtube.com/watch?v=SZi7VV1jd1o"],
+  ["07.12", "大会", "エイエイエス", "三郷38面", "○", "https://youtu.be/_FnZUaV_j-I"],
+  ["07.18", "大会", "岩谷産業", "三郷36面", "○", "https://youtu.be/VQdB2C4x3uI"],
+  ["07.25", "練習試合", "docomo", "夢の島9面", "×", "https://youtu.be/otIuQkTK0gY"],
+  ["08.02", "大会", "NEC", "三郷38面", "×", "https://youtu.be/o-bNuuNwP5I"],
+  ["08.08", "練習試合", "Gold Selection", "夢の島10面", "△", ""],
+  ["08.22", "練習試合", "ヤマトシステム開発", "夢の島3面", "×", ""],
+] as const;
+
+export const batters = [
+  ["#25", "古田 裕一", ".429", "4", "3", "2", "1.314"],
+  ["#16", "佐々木 陽彦", ".333", "5", "4", "1", ".718"],
+  ["#5", "田中 勝也", ".323", "23", "20", "16", ".892"],
+  ["#10", "小松 純也", ".300", "10", "6", "3", ".633"],
+  ["#8", "宮田 駿", ".297", "24", "19", "10", ".868"],
+  ["#23", "岡出 健太朗", ".294", "23", "15", "10", ".890"],
+  ["#11", "新開 和彦", ".289", "20", "13", "12", ".858"],
+  ["#0", "坪井 彬", ".267", "18", "8", "6", ".721"],
+  ["#22", "相川 淳一", ".242", "25", "15", "13", ".752"],
+  ["#2", "宮園 大生", ".211", "25", "15", "13", ".703"],
+] as const;
+
+export const pitchers = [
+  ["#19", "木下 公ニ", "1.11", "11", "2", "0", "3", "17"],
+  ["#10", "小松 純也", "2.79", "7", "2", "2", "0", "9"],
+  ["#18", "浅野 祐樹", "3.09", "20", "7", "4", "0", "30"],
+  ["#0", "坪井 彬", "7.03", "18", "2", "4", "0", "10"],
+] as const;
+
